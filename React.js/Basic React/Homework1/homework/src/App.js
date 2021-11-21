db9867df47e802a5ac5ef833959112cc5a33955b
Werayootk <werayoot5800@gmail.com>
2021-11-21 13:34:25 +0700

    add

--- React.js/Basic React/Homework1/homework/src/App.js
+++ React.js/Basic React/Homework1/homework/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { SignUp } from "./component/SignUp";
+import { Home } from "./component/Home";
+import { SignIn } from "./component/SignIn";
+import { CreatePikka } from "./component/CreatePikka";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/create" element={<CreatePikka />} />
+      </Routes>
     </div>
   );
 }
